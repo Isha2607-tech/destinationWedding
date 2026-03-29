131b26701cc4dbd9d8cc231477167719f4bc0581
Isha2607-tech <isha30066@gmail.com>
2026-03-29 15:08:48 +0530

    vendor login signup pages done

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,21 @@ import VenueDetailPage from "./modules/wedding/views/VenueDetailPage";
 import WeddingEnquiryPage from "./modules/wedding/views/WeddingEnquiryPage";
 import NotFound from "./app-pages/NotFound.jsx";
 import Welcome from "./app-pages/Welcome.jsx";
+import VendorLayout from "./modules/vendor/components/VendorLayout";
+import Step1BasicInfo from "./modules/vendor/onboarding/views/Step1BasicInfo";
+import Step2Portfolio from "./modules/vendor/onboarding/views/Step2Portfolio";
+import Step3Services from "./modules/vendor/onboarding/views/Step3Services";
+import Step4Pricing from "./modules/vendor/onboarding/views/Step4Pricing";
+import ReviewSubmit from "./modules/vendor/onboarding/views/ReviewSubmit";
+import VendorDashboard from "./modules/vendor/dashboard/views/VendorDashboard";
+import VendorServices from "./modules/vendor/services/views/VendorServices";
+import VendorPortfolio from "./modules/vendor/portfolio/views/VendorPortfolio";
+import VendorLeads from "./modules/vendor/leads/views/VendorLeads";
+import LeadDetails from "./modules/vendor/leads/views/LeadDetails";
+
+import VendorSignup from "./modules/vendor/auth/views/VendorSignup";
+import VendorLogin from "./modules/vendor/auth/views/VendorLogin";
+import { AuthProvider } from "./modules/vendor/context/AuthContext";
 
 const queryClient = new QueryClient();
 
@@ -56,6 +71,26 @@ const App = () => (
             <Route path="real-weddings/by-location/:destinationId" element={<RealWeddingsByLocation />} />
             <Route path="real-weddings/gallery/:weddingId" element={<RealWeddingGalleryPage />} />
           </Route>
+          
+          <Route path="/vendor/*" element={<AuthProvider><Routes>
+            <Route element={<VendorLayout />}>
+              <Route index element={<Navigate to="/vendor/login" replace />} />
+              <Route path="onboarding/step-1" element={<Step1BasicInfo />} />
+              <Route path="onboarding/step-2" element={<Step2Portfolio />} />
+              <Route path="onboarding/step-3" element={<Step3Services />} />
+              <Route path="onboarding/step-4" element={<Step4Pricing />} />
+              <Route path="onboarding/review" element={<ReviewSubmit />} />
+              <Route path="dashboard" element={<VendorDashboard />} />
+              <Route path="services" element={<VendorServices />} />
+              <Route path="portfolio" element={<VendorPortfolio />} />
+              <Route path="leads" element={<VendorLeads />} />
+              <Route path="leads/:id" element={<LeadDetails />} />
+            </Route>
+            {/* Auth routes outside of layout but inside AuthProvider */}
+            <Route path="login" element={<VendorLogin />} />
+            <Route path="signup" element={<VendorSignup />} />
+          </Routes></AuthProvider>} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
